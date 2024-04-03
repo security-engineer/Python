f38a24f3f39f251fb5c15169a761eb9eebef5e2d
TomGorani <int_kimty@naver.com>
2024-04-03 12:32:56 +0900

    수업 퀴즈2

--- list.py
+++ list.py
@@ -1,6 +1,20 @@
-i = (int)(input("정수를 입력하세요: "))
+# i = (int)(input("정수를 입력하세요: "))
 
-if i % 2 == 0 :
-    print("입력한 정수는 짝수 입니다.")
-else :
-    print("입력한 정수는 홀수 입니다.")
+# if (i % 2 == 0) :
+#     print("입력한 정수는 짝수 입니다.")
+#     print("sd")
+# else :
+#     print("입력한 정수는 홀수 입니다.")
+
+# id = ["hong", "kim", "tae"]
+# pw = 12345678
+# input_id = input("아이디를 입력하세요: ")
+
+# if (input_id in id) :
+#     input_pw = (int)(input("패스워드를 입력하세요: "))    
+#     if (pw == input_pw) :
+#         print("환영합니다!")
+#     else :
+#         print("패스워드가 틀렸습니다.")
+# else :
+#     print("아이디가 틀렸습니다.")
