89ecf76718683c1b57f195ef74ff851b8843a27d
TomGorani <int_kimty@naver.com>
2024-04-12 13:05:41 +0900

    solved about turtle module

---
+++ for.py
@@ -0,0 +1,26 @@
+
+
+# count = (int)(input("정수를 입력하시오:"));
+
+# for i in range(0,101,10):
+#     sum *= i;
+
+# print(sum);
+# # print(sum)
+
+import turtle;
+
+screen = turtle.Screen()
+t = turtle.Turtle()
+
+i = 20;
+count = 0;
+for i in range(count,31):
+    i += 3*count;
+    t.forward(i);
+    t.right(24);
+
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.mainloop()
