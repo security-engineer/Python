fb6e75a42807f35b8b8880c080ba3363203fd57d
TomGorani <int_kimty@naver.com>
2024-04-19 10:10:34 +0900

    def

--- test.py
+++ test.py
@@ -1,19 +1,36 @@
-import turtle;
+# import turtle;
 
-t = turtle.Pen()
+# t = turtle.Pen()
 
-t.pencolor("black")
-command = input("왼쪽=left, 오른쪽=right, 종료=quit :")
+# t.pencolor("black")
+# command = input("왼쪽=left, 오른쪽=right, 종료=quit :")
 
-while (1) :
-    if(command == "left") :
-        t.left(60)
-        t.forward(50)
-    elif(command == "right") :
-        t.right(60)
-        t.forward(50)
-    elif(command == "quit") :
-        break
+# while (1) :
+#     if(command == "left") :
+#         t.left(60)
+#         t.forward(50)
+#         command = input("왼쪽=left, 오른쪽=right, 종료=quit :")
+#     elif(command == "right") :
+#         t.right(60)
+#         t.forward(50)
+#         command = input("왼쪽=left, 오른쪽=right, 종료=quit :")
+#     elif(command == "quit") :
+#         break
 
 
-turtle.done()
+# turtle.done()
+
+def say_hello (a) :
+    print("안녕, ", a);
+
+say_hello("철수")
+
+# str = input("문자열을 입력하시오: ")
+# result = ""
+
+# i=0
+# for c in str :
+#     if c not in 'aeiou' :
+#         result += c
+        
+# print(result)
